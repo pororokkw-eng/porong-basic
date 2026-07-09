df904228dbafe18ee3819f7265924f714310958a
꼬마펭귄 뽀로로 <pororo.kkw@gmail.com>
2026-07-09 23:37:39 +0900

    Initial

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace porong_basic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
